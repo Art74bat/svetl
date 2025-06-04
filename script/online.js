00dd9fa9f792ba6f77d44bf93bd9121347bfeaae
Артур Бат-Очир <dumparcher@mail.ru>
2025-06-04 21:26:55 +0300

    onlin&review_js

--- script/online.js
+++ script/online.js
@@ -1,7 +1,7 @@
 const form_btn = document.querySelector('.form__btn');
-
+const form = document.forms.my_form;
+const message = document.querySelector('.message');
 function ValidPhone() {
-
     let re = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
     let myPhone = document.querySelector('.form__input_tel').value;
     let valid = re.test(myPhone);
@@ -10,16 +10,24 @@ function ValidPhone() {
     document.querySelector('.message').innerHTML=output;
     return valid;
 } 
-function ValidName() {
 
+function ValidName() {
     let re = /^[а-яА-ЯёЁa-zA-Z0-9]+$/;
     let myName = document.querySelector('.form__input_text').value;
     let valid = re.test(myName);
     if (valid) {output = ''}
     else {output = 'Проверьте введённое имя!'};
-    document.querySelector('.message').innerHTML=output;
+    message.innerHTML=output;
     return valid;
 } 
 
-form_btn.addEventListener('click', ValidPhone);
-form_btn.addEventListener('click', ValidName);
+form_btn.addEventListener('click', function(){
+    if(ValidName() && ValidPhone()){
+        form.reset();
+        message.classList.toggle('info');
+        message.textContent = 'Спасибо';
+    }else{
+        message.classList.remove('info');
+        event.preventDefault();
+    }
+});
--- script/review.js
+++ script/review.js
@@ -2,22 +2,61 @@
 let container = document.querySelector('.all_reviews_cards_wrapp');
 let btn = document.querySelector('.review_form__btn')
 let form = document.forms.my_form;
+let message = document.querySelector('.message')
 
+form.addEventListener('submit', function(){
+    if(ValidName() && ValidCity() && ValidReview()){
+        getFormValue();
+        message.textContent = '';
+    }else{
+        event.preventDefault();
+    }
+});
 
-form.addEventListener('submit', getFormValue);
-// btn.addEventListener('click',myAdd);
+function ValidName() {
+    let re = /^[а-яА-ЯёЁa-zA-Z0-9]+$/;
+    let myName = form.elements.my_name.value;
+    let valid = re.test(myName);
+    if(!valid){
+       message.textContent = 'Проверьте введённое имя!';
+    }
+    return valid;
+} 
+function ValidCity() {
+    let re = /^[а-яА-ЯёЁa-zA-Z0-9]+$/;
+    let myCity = form.elements.city.value;
+    let valid = re.test(myCity);
+    if(!valid){
+       message.textContent = 'Проверьте название города!';
+    }
+    return valid;
+} 
+function ValidReview() {
+    let re = /^[а-яА-ЯёЁa-zA-Z0-9]+$/;
+    let myReview = form.elements.review.value;
+    let valid = re.test(myReview);
+    if(!valid){
+       message.textContent = 'Некорректные символы в тексте отзыва!';
+    }
+    return valid;
+} 
 
 function getFormValue(){
     event.preventDefault();
-    const form = document.forms.my_form,
-    name = form.elements.name,
+    my_name = form.elements.my_name,
     city = form.elements.city,
     review = form.elements.review;
     const data = {
-        name:name.value,
+        name:my_name.value,
         city:city.value,
         review:review.value,
     };
+
+    // console.log(data);
+
+    // создать карту отзыва-------------------------
+
+
     let card = document.createElement('div'); 
     card.className = "all_reviews_card"; 
 
@@ -34,15 +73,13 @@ function getFormValue(){
 
     let cardName = document.createElement('span');
     cardName.className = "all_reviews_card__title_name"; 
-    cardName.textContent = data.name;
+    cardName.textContent = data.name ;
     cardTitle.appendChild(cardName);
 
     let cardCity = document.createElement('span');
     cardCity.className = "all_reviews_card__title_city"; 
-    cardCity.textContent = data.city;
+    cardCity.textContent = "г."+data.city;
     cardTitle.appendChild(cardCity);
-
-    console.log(data);
 
     let cardText = document.createElement('p');
     cardText.className = "all_reviews_card__descript"; 
@@ -50,46 +87,9 @@ function getFormValue(){
     card.appendChild(cardText);
 
     container.appendChild(card);
-
-
+    
+    form.reset();
 }
-
-
-// function myAdd(data){
-//     let card = document.createElement('div'); 
-//     card.className = "all_reviews_card"; 
-
-//     let cardDate = document.createElement('span');
-//     cardDate.className = "all_reviews_card__date"; 
-//     cardDate.textContent = '20.05.2025';
-//     card.appendChild(cardDate);
-
-
-
-//     let cardTitle = document.createElement('h2');
-//     cardTitle.className = "all_reviews_card__title"; 
-//     card.appendChild(cardTitle);
-
-//     let cardName = document.createElement('span');
-//     cardName.className = "all_reviews_card__title_name"; 
-//     cardName.textContent = data.name;
-//     card.appendChild(cardTitle);
-
-//     let cardCity = document.createElement('span');
-//     cardCity.className = "all_reviews_card__title_city"; 
-//     cardCity.textContent = data.city;
-//     card.appendChild(cardTitle);
-
-
-   
-
-//     let cardText = document.createElement('p');
-//     cardText.className = "all_reviews_card__descript"; 
-//     cardText.textContent = data.review;
-//     card.appendChild(cardText);
-
-//     container.appendChild(card);
-// }
 
 
 
